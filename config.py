0946cb9a947a1363e0cd607c099de3047d2e05e3
Tushar R Singh <tusharsingh222555@gmail.com>
2025-12-26 00:26:25 +0530

    Vercel deployment setup (ignoring .env)

--- config.py
+++ config.py
@@ -10,6 +10,13 @@ class Config:
         'mysql+pymysql://root@localhost/nst_prep_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
+    # Engine options for Aiven SSL support
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "connect_args": {
+            "ssl": {"check_hostname": False} if os.environ.get('DATABASE_URL') and 'aivencloud.com' in os.environ.get('DATABASE_URL') else None
+        }
+    }
+    
     # Razorpay Configuration
     RAZORPAY_KEY_ID = os.environ.get('RAZORPAY_KEY_ID')
     RAZORPAY_KEY_SECRET = os.environ.get('RAZORPAY_KEY_SECRET')
