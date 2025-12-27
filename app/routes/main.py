91317803c460b386dd0090d5e63bf0525502dc76
Tushar R Singh <tusharsingh222555@gmail.com>
2025-12-27 21:31:47 +0530

    Policies pages

--- app/routes/main.py
+++ app/routes/main.py
@@ -6,3 +6,18 @@ bp = Blueprint('main', __name__)
 def index():
     """Landing page"""
     return render_template('index.html')
+
+@bp.route('/terms')
+def terms():
+    """Terms and Conditions"""
+    return render_template('policy/terms.html')
+
+@bp.route('/refund-policy')
+def refund_policy():
+    """Cancellation and Refund Policy"""
+    return render_template('policy/refund.html')
+
+@bp.route('/privacy-policy')
+def privacy_policy():
+    """Privacy Policy"""
+    return render_template('policy/privacy.html')
