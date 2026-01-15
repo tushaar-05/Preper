12f6b465ca05a49f972d1f5e9c27c21fcddb80e6
DevanshSainiji <sainidevansh1308@gmail.com>
2026-01-15 09:58:25 +0530

    corrected safari problems

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     # XAMPP MySQL default configuration (root user with no password)
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql+pymysql://root@localhost/nst_prep_db'
+        'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # Engine options for Aiven SSL support
