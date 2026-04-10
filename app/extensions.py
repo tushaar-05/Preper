c95dd857b1df3cf78e9a0205be48f2ed4365d692
DevanshSainiji <sainidevansh1308@gmail.com>
2026-04-11 00:49:59 +0530

    Revert "made the codebase interview ready"
    This reverts commit db19e4a5528a12302febb0ceada1eb1fd162e204.

--- app/extensions.py
+++ app/extensions.py
@@ -3,12 +3,7 @@ from flask_migrate import Migrate
 from flask_caching import Cache
 from flask_compress import Compress
 
-from datetime import timedelta, timezone
-
 db = SQLAlchemy()
 migrate = Migrate()
 cache = Cache()
 compress = Compress()
-
-# Global IST Timezone
-IST = timezone(timedelta(hours=5, minutes=30))
